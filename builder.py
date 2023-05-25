73a48b445c1f80c685409b09a84ef1216a3dfd10
guch8017 <guch8017@gmail.com>
2023-05-25 10:56:46 +0800

    Add multiprocessing support

--- builder.py
+++ builder.py
@@ -1,3 +1,5 @@
+import multiprocessing
+
 try:
     from .utils import xy_rotate_box
     from .ocr import OCREngine
@@ -5,8 +7,15 @@ except ImportError:
     from utils import xy_rotate_box
     from ocr import OCREngine
 import cv2
-from tqdm import tqdm
 import numpy as np
+from multiprocessing import Pool, Manager
+from functools import partial
+
+
+def _ocr(cell: dict, engine: OCREngine, image: np.ndarray):
+    p1, p2 = cell['box']
+    text = engine.ocr(image[p1[1]:p2[1], p1[0]:p2[0], :])
+    cell['text'] = text
 
 
 class TableBuilder:
@@ -14,7 +23,7 @@ class TableBuilder:
     表格重建
     """
 
-    def __init__(self, ceil_boxes, image, interval=10):
+    def __init__(self, ceil_boxes, image, interval=10, engine=None):
         """
         :param ceil_boxes: [[x0,y0,x1,y1,x2,y2,x3,y3,x4,y4]]
         :param image: Original image
@@ -24,6 +33,7 @@ class TableBuilder:
         self.ceil_boxes = ceil_boxes
         self.diag_boxes = [[int(x[0]), int(x[1]), int(x[4]), int(x[5])] for x in ceil_boxes]
         self.interval = interval
+        self.engine = engine
         self.batch()
 
     def batch(self):
@@ -31,7 +41,9 @@ class TableBuilder:
         row_cor, row_ind = self.table_line_cor(self.diag_boxes, axis='row', interval=self.interval)
         col_cor, col_ind = self.table_line_cor(self.diag_boxes, axis='col', interval=self.interval)
         # row: [r0, r1], col: [c0, c1], box: [TL, BR]
-        cor = [{'row': line[1], 'col': line[0], 'box': [(row_ind[line[0][0]], col_ind[line[1][0]]), (row_ind[line[0][1]], col_ind[line[1][1]])]} for line in zip(row_cor, col_cor)]
+        cor = [{'row': line[1], 'col': line[0],
+                'box': [(row_ind[line[0][0]], col_ind[line[1][0]]), (row_ind[line[0][1]], col_ind[line[1][1]])]} for
+               line in zip(row_cor, col_cor)]
         self.cor = cor
 
     @staticmethod
@@ -69,12 +81,34 @@ class TableBuilder:
             cor = [[edges_map_index[edges_map[line[0]]], edges_map_index[edges_map[line[2]]]] for line in lines]
         return cor, ind2edges
 
-    def do_ocr(self, engine: OCREngine):
+    def set_ocr_engine(self, engine: OCREngine):
+        self.engine = engine
+
+    def do_ocr(self, process=multiprocessing.cpu_count()):
+        """
+        :param process: Number of processes use to run OCR
+        When process is set to 1, it will use a for-loop
+        When process is larger than 1, it will use multiprocessing.Pool & Map
+        """
+        if self.engine is None:
+            raise Exception('OCR engine is not set')
+        if not isinstance(process, int) and process < 1:
+            raise ValueError('Process must be a positive integer')
         print('Running ocr...')
-        for cell in tqdm(self.cor):
-            p1, p2 = cell['box']
-            text = engine.ocr(self.image[p1[1]:p2[1], p1[0]:p2[0], :])
-            cell['text'] = text
+        if process == 1:
+            for cell in self.cor:
+                p1, p2 = cell['box']
+                text = self.engine.ocr(self.image[p1[1]:p2[1], p1[0]:p2[0], :])
+                cell['text'] = text
+        else:
+            manager = Manager()
+            cor = manager.list([manager.dict(dt) for dt in self.cor])
+            with Pool() as pool:
+                pool.map(partial(_ocr, engine=self.engine, image=self.image), cor)
+                pool.close()
+                pool.join()
+            self.cor = [dict(dt) for dt in cor]
+            manager.shutdown()
 
     def to_excel(self):
         import xlwt
@@ -82,7 +116,7 @@ class TableBuilder:
         workbook = xlwt.Workbook()
         if len(self.cor) == 0:
             worksheet = workbook.add_sheet('table')
-            worksheet.write_merge(0, 0, 0, 0, "无数据")
+            worksheet.write_merge(0, 0, 0, 0, "No Data")
         else:
             worksheet = workbook.add_sheet('page')
             page_row = 0
